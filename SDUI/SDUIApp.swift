50f6a4e113b60e1626dd1f32ba1399e4f8c9cc19
JeanCamargoReal <jeancamargoreal@icloud.com>
2025-03-22 19:19:51 -0300

    Initial Commit

---
+++ SDUI/SDUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDUIApp.swift
+//  SDUI
+//
+//  Created by Jean Camargo on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SDUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
